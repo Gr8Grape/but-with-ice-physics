130e3137ae881b5d1c7b9d538f397335ecf8a067
Gr8Grape <evan@unnamed.net>
2025-12-15 22:27:37 -0600

    Dash
    Added dash. click in a direction you want to move in, and it will move you a speed equal to the dash mult times your normal speed.

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 @export var strength := 50.0
 @export var accel := 5.0
 @export var bounciness := .7
+@export var dash_mult := 2
 
 var dir : Vector2
 var force_push : float
@@ -17,9 +18,14 @@ func calc_dir() -> void:
 func calc_force_push() -> void:
 	force_push = strength + velocity.length() 
 
+func handle_input() -> void:
+	if Input.is_action_just_pressed("eject"):
+		velocity = speed * dash_mult * global_position.direction_to(get_global_mouse_position())
+
 func _physics_process(delta: float) -> void:
 	calc_force_push()
 	calc_dir()
+	handle_input()
 	
 	if dir:
 		velocity = lerp(velocity, dir * speed, accel * delta)
