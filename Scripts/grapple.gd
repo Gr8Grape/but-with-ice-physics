183a7b8ea84cfd0f0070d9c5c9d696b8fb05a5b4
Gr8Grape <evan@unnamed.net>
2025-12-18 15:52:46 -0600

    Movement update
    Adjusted values, code, etc. to make movement slightly better.
    Added coasting, sprinting and slowing.
    Coasting lets the ball roll, slowing slows the ball, and sprint is sprinting.
    Grappling to an object now makes it stick to the correct point of contact, rather than snapping to the center.
    Hook now no longer extends indefinitely.

--- Scripts/grapple.gd
+++ Scripts/grapple.gd
@@ -6,16 +6,18 @@ extends Node2D
 var direction := Vector2(0,0)
 #global position the head is in
 var glob_head := Vector2(0,0)
-var def_speed := 300.0
+#def_speed is the default speed
+#speed can change so we need a reference point
+var def_speed := 1000
 var speed : float
 var flying := false
 var hooked := false
-var obj_hooked
+var obj_hooked : Node2D
 
-func shoot(dir: Vector2, init: float) -> void:
+func shoot(dir : Vector2) -> void:
+	speed = def_speed
 	obj_hooked = null
-	speed = def_speed + init
-	#Normalize the direction and save it
+	#ensure the place we need the direction of is a normal vector
 	direction = dir.normalized()
 	flying = true
 	#reset position
@@ -29,7 +31,8 @@ func release() -> void:
 func _process(_delta: float) -> void:
 	#Only visible if flying or attached to something
 	self.visible = flying or hooked
-	if not self.visible:
+	if not visible:
+		#dont bother with the rest of the code, we aren't in use
 		return
 	var head_pos = to_local(glob_head)
 	#rotate the line and head to fit on the line between ourself and the head
@@ -37,20 +40,32 @@ func _process(_delta: float) -> void:
 	head.rotation = position.angle_to_point(head_pos) + deg_to_rad(90)
 	line.position = head_pos
 	line.region_rect.size.y = head_pos.length()
+	
+	#release when slow enough
+	if speed <= 50:
+		release()
 
+#Stands of (p)oint (o)f (c)ontact
+#this is where the hook head touches the object relative to the object
+var poc : Vector2
 func _physics_process(delta: float) -> void:
 	#reset heads position
 	head.global_position = glob_head
 	if flying:
-		#if collision
+		speed = move_toward(speed, 0, speed * 4 * delta)
 		var collision := head.move_and_collide(direction * speed * delta)
 		if collision:
 			hooked = true
 			flying = false
 			if collision.get_collider() is RigidBody2D:
 				obj_hooked = collision.get_collider()
+				poc = obj_hooked.to_local(head.global_position)
 	
 	if obj_hooked:
-		glob_head = obj_hooked.global_position
+		#we want the hook head to "stick" to the object
+		#so we must set the head to the position of the object
+		#we want to add the poc to global position so that it offsets
+		#the object can rotate, however, so account for that
+		glob_head = obj_hooked.global_position + poc.rotated(obj_hooked.rotation)
 	else:
 		glob_head = head.global_position
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,11 +1,18 @@
 extends CharacterBody2D
 
-@export var speed := 100.0
+#by the way chat, "calc" is short for "calculation"
+#dw im just using slang
+
+#region variables
+@export var speed := 150.0
 @export var strength := 50.0
-@export var accel := 5.0
-@export var bounciness := .7
-@export var dash_mult := 2
-@export var chain_str := 1000.0
+@export var accel := 600.0
+@export var bounciness := .4
+@export var dash_mult := 2.0
+@export var chain_str := 1500.0
+@export var sprint_mult := 1.2
+@export var slow_mult := .75
+@export var friction := 500.0
 
 @onready var grapple: Node2D = $Grapple
 
@@ -14,27 +21,45 @@ var force_push : float
 var reel_amt : int = 0
 var reel_strength : float = 1
 var chain_velocity := Vector2.ZERO
+var speed_mult : float
+#endregion
+
+#region calc functions
 
 #get direction for x and y based on whether up/down, left/right is pressed.
 func calc_dir() -> void:
 	dir = Vector2(Input.get_axis("left", "right"), Input.get_axis("up", "down"))
 
-#this val is calculated based on speed.
-#strength is the base value that the player can push
+#calc the force used to push objects
 func calc_force_push() -> void:
-	force_push = strength + velocity.length() 
+	#this val is calculated based on speed.
+	#strength is the base value that the player can push
+	#if player is more bouncy, not a lot of force is transfered
+	force_push = strength + (velocity.length() * (1 - bounciness))
 
 func mouse_dir() -> Vector2:
 	return global_position.direction_to(get_global_mouse_position())
 
+#endregion
+
 func handle_input() -> void:
-	if Input.is_action_just_pressed("eject"):
-		velocity = speed * dash_mult * mouse_dir()
+	#only dash while not coasting
+	if Input.is_action_just_pressed("dash") and not Input.is_action_pressed("coast"):
+		velocity = speed * speed_mult * dash_mult * mouse_dir()
 	
+	#grapplingw
 	if Input.is_action_just_pressed("grapple"):
-		grapple.shoot(get_local_mouse_position(), velocity.length())
+		grapple.shoot(get_local_mouse_position())
 	elif Input.is_action_just_released("grapple"):
 		grapple.release()
+	
+	#using elifs because slowing his priority over sprinting
+	if Input.is_action_pressed("slow"):
+		speed_mult = slow_mult
+	elif Input.is_action_pressed("sprint"):
+		speed_mult = sprint_mult
+	else:
+		speed_mult = 1
 
 func _physics_process(delta: float) -> void:
 	calc_force_push()
@@ -43,25 +68,33 @@ func _physics_process(delta: float) -> void:
 	
 	#moving
 	if dir:
-		velocity = lerp(velocity, dir * speed, accel * delta)
+		velocity = velocity.move_toward(dir * speed * speed_mult, accel * delta)
 	else:
-		velocity.x = lerp(velocity.x, 0.0, accel * delta)
-		velocity.y = lerp(velocity.y, 0.0, accel * delta)
+		#physics get kinda wonky while hooked, this makes it feel better
+		#ill be honest not sure why/how this works
+		if not grapple.hooked:
+			#let the ball roll while player coasts
+			if Input.is_action_pressed("coast"):
+				velocity = velocity.move_toward(Vector2.ZERO, friction / 3 * delta)
+			else:
+				velocity = velocity.move_toward(Vector2.ZERO, friction * delta)
 	
-	# Hook physics
+	#hook stuff
 	if grapple.hooked:
-		chain_velocity = to_local(grapple.glob_head).normalized() * chain_str * reel_strength
+		chain_velocity = to_local(grapple.glob_head).normalized() * chain_str
 	else:
-		reel_amt = 0
-		reel_strength = 1
 		chain_velocity = Vector2(0,0)
 	velocity += chain_velocity * delta
+	
+	#speed cap
+	if velocity.length() > 500:
+		velocity = velocity.normalized() * 500
 	
 	#bounce
 	var collision = move_and_collide(velocity * delta)
 	if collision:
+		if grapple.hooked:
+			grapple.release()
 		velocity = velocity.bounce(collision.get_normal()) * bounciness
 		if collision.get_collider() is RigidBody2D:
-			if grapple.hooked:
-				grapple.release()
 			collision.get_collider().apply_impulse(-collision.get_normal() * force_push)
